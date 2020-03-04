10bd4d2c838b1acd92fdcc316900ccf3f1a03e3a
Emilie Champagne <e.champagne.1@gmail.com>
2020-03-04 09:50:11 -0500

    Add files via upload
    Addition of CET data & updates to analyses

--- AnalyseChimique.R
+++ AnalyseChimique.R
@@ -6,9 +6,11 @@ library(tidyr)
 ####Données####
 PIB <- read.table("./PIB.txt", header = TRUE, dec = ",", stringsAsFactors = FALSE)
 ERS <- read.table("./ERS.txt", header = TRUE, dec = ",", stringsAsFactors = FALSE)
+CET <- read.table("./CET.txt", header = TRUE, dec = ",", stringsAsFactors = FALSE)
 
 PIB <- separate(PIB, ID_plant, into = c("Esp", "Prov", "ID"), sep = "-")
 ERS <- separate(ERS, ID_plant, into = c("Esp", "Prov", "ID"), sep = "-")
+CET <- separate(CET, ID_plant, into = c("Esp", "Prov", "ID"), sep = "-")
 
 ####Graph####
 
@@ -20,3 +22,7 @@ boxplot(ERS$Phen)
 
 boxplot(Flav ~ Prov, data= PIB)
 boxplot(ERS$Flav)
+
+boxplot(N ~ Prov, data= CET)
+boxplot(Phen ~ Prov, data= CET)
+boxplot(Flav ~ Prov, data= CET)
